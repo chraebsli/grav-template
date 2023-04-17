12a5d7007db0399e217840e4a99aa5767aa23936
chraebsli <nicholas.joel.krebs@gmail.com>
2023-04-17 09:12:57 +0200

    fix inject secrets

--- secrets.sh
+++ secrets.sh
@@ -4,6 +4,7 @@
 SALT=$(doppler secrets get SALT --plain)
 LICENSE_LIGHTBOX_GALLERY=$(doppler secrets get LICENSE_LIGHTBOX_GALLERY --plain)
 
-# Inject Environment Variables
-yq -i ".salt = \"$SALT\"" user/config/security.yaml
-yq -i ".licenses.lightbox-gallery = \"$LICENSE_LIGHTBOX_GALLERY\"" user/data/licenses.yaml
+# Set Environment Variables in .env.local
+touch .env.local
+echo "SALT=$SALT" >> .env.local
+echo "LICENSE_LIGHTBOX_GALLERY=$LICENSE_LIGHTBOX_GALLERY" >> .env.local
