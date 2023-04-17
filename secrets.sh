6374467fdfa187c88b131a7a641f9f6b7174fc60
chraebsli <nicholas.joel.krebs@gmail.com>
2023-04-17 14:11:06 +0200

    fix: config

--- secrets.sh
+++ secrets.sh
@@ -5,6 +5,7 @@ SALT=$(doppler secrets get SALT --plain)
 LICENSE_LIGHTBOX_GALLERY=$(doppler secrets get LICENSE_LIGHTBOX_GALLERY --plain)
 
 # Set Environment Variables in .env.local
+rm -f .env.local
 touch .env.local
 echo "SALT=$SALT" >> .env.local
 echo "LICENSE_LIGHTBOX_GALLERY=$LICENSE_LIGHTBOX_GALLERY" >> .env.local
