dea533ac8784ab46b2772b05a445f06161abb12b
Nicholas Krebs <nicholas.joel.krebs@gmail.com>
2023-05-05 10:40:57 +0200

    #21 Update Script

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Stash local changes
+git stash push --include-untracked -- logs/popularity user/data/flex/indexes
+
+# Reset working directory
+git reset --hard
+git clean -fd
+
+# Pull latest changes
+git pull
+
+# Restore local changes
+git stash pop 0
